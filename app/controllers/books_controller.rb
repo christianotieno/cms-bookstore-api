51f9255e98a3d7b9f8a2176563c4ac6856ca0adc
christianotieno <oduorchristian@gmail.com>
2020-06-16 01:52:07 +0300

    Implement book controller

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,36 @@
+class BooksController < ApplicationController
+  # before_action :set_book, only: [:show, :destory, :create]
+  
+  def def index
+    @books = Book.all
+    render json: @books
+  end
+  
+  def def show
+    render json: @book
+  end
+
+  def create
+    @book = Book.new(book_params)
+    
+    if @movie.save
+      render json: @book, status: :created
+    else
+      render json: @book.errors, status: :unprocessable_entity
+    end
+  end
+
+  def :destroy
+    @book.destroy
+  end
+
+
+  private
+  # def set_book
+  #   @book = Book.find(params[:id])
+  # end
+
+  def book_params
+    params.require(:book).permit(:title, :author, :category)
+  end
+end
